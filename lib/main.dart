9c9336bebb768b65b692bea963bc5b337155261b
Alvaro Armijos <alvaro.armijoss@gmail.com>
2023-11-20 18:27:02 -0500

    Inject HomeBloc on Main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:bluey_app/data/firebase_api/firebase_api_client.dart';
+import 'package:bluey_app/features/home/bloc/home_bloc.dart';
 import 'package:bluey_app/features/onboarding/page/onboarding_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +14,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: OnboardingPage());
+    return BlocProvider(
+      create: (_) => HomeBloc(FirebaseApiClient(httpClient: http.Client())),
+      child: const MaterialApp(
+        home: OnboardingPage(),
+      ),
+    );
   }
 }
