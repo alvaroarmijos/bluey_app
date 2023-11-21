4ea5f7749816d55318175666998353e84379429c
Alvaro Armijos <alvaro.armijoss@gmail.com>
2023-11-20 23:34:53 -0500

    Add fromJson and fromJsonArray methods on Models.

--- lib/features/character_detail/model/character.dart
+++ lib/features/character_detail/model/character.dart
@@ -13,6 +13,25 @@ class Character {
       required this.description,
       required this.descriptionImg,
       required this.items});
+
+  factory Character.fromJson(Map jsonObj) {
+    final String name = jsonObj['name'] ?? "";
+    final String quote = jsonObj['quote'] ?? "";
+    final String img = jsonObj['img'] ?? "";
+    final String description = jsonObj['description'] ?? "";
+    final String descriptionImg = jsonObj['descriptionImg'] ?? "";
+    final List<ItemDescription> items =
+        ItemDescription.fromJsonArray(jsonObj['items']);
+
+    return Character(
+      name: name,
+      quote: quote,
+      img: img,
+      description: description,
+      descriptionImg: descriptionImg,
+      items: items,
+    );
+  }
 }
 
 class ItemDescription {
@@ -20,4 +39,17 @@ class ItemDescription {
   final String description;
 
   ItemDescription({required this.label, required this.description});
+
+  factory ItemDescription.fromJson(Map jsonObj) {
+    final String label = jsonObj['label'] ?? "";
+    final String description = jsonObj['description'] ?? "";
+
+    return ItemDescription(
+      label: label,
+      description: description,
+    );
+  }
+
+  static List<ItemDescription> fromJsonArray(List jsonArray) =>
+      jsonArray.map((e) => ItemDescription.fromJson(e)).toList();
 }
